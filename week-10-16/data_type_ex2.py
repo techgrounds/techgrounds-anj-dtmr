f943dfee261def92912d1dbc448614d86c1c7821
adtmr <anjdeitmer@gmail.com>
2023-05-30 11:21:11 +0200

    Create data_type_ex2.py

---
+++ week-10-16/data_type_ex2.py
@@ -0,0 +1,5 @@
+# Exercise 2:
+# ● Create a new script.
+# ● Use the input() function to get input from the user. Store that input in a variable.
+# ● Find out what data type the output of input() is. See if it is different for different kinds
+# of input (numbers, words, etc.).
