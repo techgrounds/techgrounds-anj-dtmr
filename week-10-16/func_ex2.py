91ed9fede40c810e4820a6b77d165b6f6dd07bc0
adtmr <anjdeitmer@gmail.com>
2023-05-30 15:11:28 +0200

    Create func_ex2.py

---
+++ week-10-16/func_ex2.py
@@ -0,0 +1,20 @@
+# Exercise 2:
+# ● Create a new script.
+# ● Write a custom function myfunction() that prints “Hello, world!” to the terminal. Call
+# myfunction.
+
+def myfunction():
+    print("Hello, world!")
+
+
+myfunction()
+
+# ● Rewrite your function so that it takes a string as an argument. Then, it should print
+# “Hello, <string>”.
+
+
+def myfunction(str):
+    print(f"Hello, {str}")
+
+
+myfunction("Anj")
