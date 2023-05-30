7b9bd7398ab7e4049b089456139bfb349d547143
adtmr <anjdeitmer@gmail.com>
2023-05-30 12:43:43 +0200

    Create loop_ex3.py

---
+++ week-10-16/loop_ex3.py
@@ -0,0 +1,10 @@
+# Exercise 3:
+# Create a new script.
+# Copy the array below into your script.
+#       `arr = ['Coen', 'Casper', 'Joshua', 'Abdessamad', 'Saskia']`
+# Use a for loop to loop over the array. Print every name individually.
+
+arr = ['Coen', 'Casper', 'Joshua', 'Abdessamad', 'Saskia']
+
+for i in arr:
+    print(i)
