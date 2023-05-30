514688706b8bcc18fdd0848ddc6063025052d598
adtmr <anjdeitmer@gmail.com>
2023-05-30 12:43:51 +0200

    Create loop_ex1.py

---
+++ week-10-16/loop_ex1.py
@@ -0,0 +1,15 @@
+# Exercise 1:
+# ● Create a new script.
+# ● Create a variable x and give it the value 0.
+x = 0
+
+# ● Use a while loop to print the value of x in every iteration 
+# of the loop. After printing, the value of x should increase by 1. The loop should run as long as x 
+# is smaller than or equal to 10.
+
+n = 10
+
+while x <= n:
+    print(x)
+    x = x + 1
+
