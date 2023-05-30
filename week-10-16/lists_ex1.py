78c3649095029033ae33438cb91bf9ab77117fe5
adtmr <anjdeitmer@gmail.com>
2023-05-30 15:19:17 +0200

    Create lists_ex1.py

---
+++ week-10-16/lists_ex1.py
@@ -0,0 +1,11 @@
+# Exercise 1:
+# ● Create a new script.
+# ● Create a variable that contains a list of five names.
+# ● Loop over the list using a for loop. Print every individual name in the list on a new
+# line.
+
+names = ["Shake", "Den", "Keef", "Tasya", "Lottie"]
+
+for i in names:
+    print(i)
+    
