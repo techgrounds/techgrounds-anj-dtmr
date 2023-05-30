a741a81a3738cc256dcd2c5ea4c18d83d82e3540
adtmr <anjdeitmer@gmail.com>
2023-05-30 11:21:25 +0200

    Create variables_ex1.py

---
+++ week-10-16/variables_ex1.py
@@ -0,0 +1,13 @@
+# Exercise 1:
+# ● Create a new script.
+# ● Create two variables x and y. Assign a numerical value to both variables.
+# ● Print the values of x and y.
+# ● Create a third variable named z. The value of z should be the sum of x and y.
+# ● Print the value of z.
+
+var_x = 5
+var_y = 10
+
+var_z = var_x + var_y
+
+print(var_z)
