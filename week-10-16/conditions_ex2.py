e2ea01d4207f43cdec0b21a50ed1fe0bfc7e175f
adtmr <anjdeitmer@gmail.com>
2023-05-30 14:20:03 +0200

    Create conditions_ex2.py

---
+++ week-10-16/conditions_ex2.py
@@ -0,0 +1,24 @@
+# Exercise 2:
+# ● Create a new script.
+# ● Ask the user of your script for a number. Give them a response based on whether the
+# number is higher than, lower than, or equal to 100.
+# ● Make the game repeat until the user inputs 100.
+
+answer = 100
+question = "Guess a number between 0-1000:"
+
+while True:
+    usr_num = int(input(question))
+
+    if usr_num > answer:
+        print("This is higher, choose lower number")
+    elif usr_num < answer:
+        print("This is lower, choose higher number")
+        continue
+    else:
+        print(f"{usr_num} is just the right number")
+        break
+
+
+
+
