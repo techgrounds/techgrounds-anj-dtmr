aed580c34a286a87a1350f71760f19727af2e926
adtmr <anjdeitmer@gmail.com>
2023-05-30 11:31:11 +0200

    Update data_type_ex2.py

--- week-10-16/data_type_ex2.py
+++ week-10-16/data_type_ex2.py
@@ -1,5 +1,14 @@
 # Exercise 2:
 # ● Create a new script.
-# ● Use the input() function to get input from the user. Store that input in a variable.
-# ● Find out what data type the output of input() is. See if it is different for different kinds
+# ● Use the input() function to get input from the user. Store that input 
+# in a variable.
+# ● Find out what data type the output of input() is. See if it is 
+# different for different kinds
 # of input (numbers, words, etc.).
+
+name = input("Type your name: ")
+age = input("Type your age: ")
+
+print(name, "data type:", type(name))
+# print(age, "data type:", type(age))
+print(age, "data type:", type(int(age)))
