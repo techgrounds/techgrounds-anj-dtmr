06baf92895ef0fd17191513d002f8a6f8b7bfe3a
adtmr <anjdeitmer@gmail.com>
2023-05-30 11:21:31 +0200

    Create variables_ex3.py

---
+++ week-10-16/variables_ex3.py
@@ -0,0 +1,16 @@
+# Exercise 3:
+# ● Create a new script.
+# ● Create a variable and assign a value to it.
+# ● Print the text “Value 1: <value 1 here>”.
+# ● Change the value of that same variable.
+# ● Print the text “Value 2: <value 2 here>”.
+# ● Change the value of that same variable.
+# ● Print the text “Value 3: <value 3 here>”.
+
+var1 = "Fruits"
+var2 = 3
+var3 = "Yes I get the point"
+
+print("Value 1: " + var1)
+print("Value 2: " + str(var2))
+print("Value 3: " + var3)
