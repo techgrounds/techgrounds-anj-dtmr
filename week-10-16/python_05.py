615b0b7292fd6cd5587d45d7ab09422ef0d990a9
adtmr <anjdeitmer@gmail.com>
2023-05-30 14:20:09 +0200

    Create python_05.py

---
+++ week-10-16/python_05.py
@@ -0,0 +1,58 @@
+# Conditions
+
+## Key terminology
+
+- [ ]
+
+## Benodigdheden
+
+- [x] Python
+- [x] VS Code
+
+## Opdrachten
+
+Exercise 1:
+- [x] Create a new script.
+- [x] Use the input() function to ask the user of your script for their name. If the name they
+input is your name, print a personalized welcome message. If not, print a different
+personalized message.
+
+[Check what I did]()
+
+Exercise 2:
+- [x] Create a new script.
+- [x] Ask the user of your script for a number. Give them a response based on whether the
+number is higher than, lower than, or equal to 100.
+- [x] Make the game repeat until the user inputs 100.
+
+
+[Take a peek]()
+
+## Sources list used for solving the exercise
+
+- [Notes]()
+- [Flashcard]()
+
+## Overcome challenges:
+
+Short description of the challeges encountered, and how I solved them:
+
+Issue 1: Asking better questions
+
+- [ ]
+- [ ]
+
+Issue 2:
+Issue 3:
+Issue 4:
+Issue 5:
+Issue 6:
+
+## Results
+
+Brief description of the result of the exercises. An image can speak more than a thousand words.
+
+**Here's the step by step guide how I did it::**
+
+**Description:**
+![Label]()
