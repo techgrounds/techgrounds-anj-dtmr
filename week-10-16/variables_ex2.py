e287ec2573a3adc6b2c09343bca2e4c5612febf3
adtmr <anjdeitmer@gmail.com>
2023-05-30 11:21:28 +0200

    Create variables_ex2.py

---
+++ week-10-16/variables_ex2.py
@@ -0,0 +1,8 @@
+# Exercise 2:
+# ● Create a new script.
+# ● Create a variable name. The value of name should be your name.
+# ● Print the text “Hello, <your name here>”. Use name in the print statement.
+
+name = "Anj"
+
+print("Hello " + name)
