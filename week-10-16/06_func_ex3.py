f99ba5fbdddd7524d9abd2e8ffa46544aff31c1e
adtmr <anjdeitmer@gmail.com>
2023-05-31 12:17:44 +0200

    Update 06_func_ex3.py

--- week-10-16/06_func_ex3.py
+++ week-10-16/06_func_ex3.py
@@ -7,9 +7,9 @@
 # x = 128
 # y = 255
 # z = avg(x,y)
+# print("The average of", x, "and", y, "is", z)
 # ● Write the custom function avg() so that it returns the average of the given
 # parameters. You are not allowed to edit any code below the second comment.
-
 
 def avg(a, b):
     # # write your code here
