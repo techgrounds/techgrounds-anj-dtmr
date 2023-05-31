7006eaca89341a6728a097a607fd54eae8579f3c
adtmr <anjdeitmer@gmail.com>
2023-05-31 14:52:57 +0200

    Create 09_python_num_guess.py

---
+++ week-10-16/09_python_num_guess.py
@@ -0,0 +1,5 @@
+# Number Guessing:
+# ● Generate a random number between 1 and 100 (or any other range).
+# ● The player guesses a number. For every wrong answer the player receives a clue.
+# ● When the player guesses the right number, display a score.
+
