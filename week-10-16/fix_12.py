d5e084011969eed2319d184655844206a5d8199b
adtmr <anjdeitmer@gmail.com>
2023-06-02 11:32:57 +0200

    Create fix_12.py

---
+++ week-10-16/fix_12.py
@@ -0,0 +1,31 @@
+# '''
+# The output should be:
+# 4
+# 16129
+# '''
+# def square(x):
+# 	return x**2
+#
+# nr = square(2)
+# print(nr)
+#
+# big = square(foo)
+# print(big)
+#
+# foo = 127
+
+
+def square(x):
+    return x**2
+
+
+nr = square(2)
+print(nr)
+
+
+foo = 127
+big = square(foo)
+print(big)
+
+
+
