abd5faa455794638b6ad70009f59fb09475cfd7b
adtmr <anjdeitmer@gmail.com>
2023-06-02 11:33:17 +0200

    Create fix_16.py

---
+++ week-10-16/fix_16.py
@@ -0,0 +1,73 @@
+# '''
+# The output should be:
+#
+# '''
+# import random
+#
+# # generate random int
+# goal = random.randint(1,100)
+#
+# win = False
+# tries = 0
+#
+# while win == False and tries < 7:
+# 	try:
+# 		# ask for input
+# 		inpt = int(input("Please input a number between 1 and 100: "))
+# 		# count attempt as a try
+# 		tries += 1
+#
+# 		# check for match
+# 		if inpt == goal:
+# 			win = True
+# 			print("Congrats, you guessed the number!")
+# 			print("It took you", tries, "tries")
+# 		# give hints
+# 		elif inpt < goal:
+# 			print("The number should be higher")
+# 		else:
+# 			print("The number should be lower")
+#
+# 	except:
+# 		print("Please type an integer")
+#
+# #
+# if win == False:
+# 	print("Game over! You took more than seven tries")
+
+import random
+
+# generate random int
+goal = random.randint(1, 100)
+# print(goal)
+win = False
+tries = 0
+
+while not win and tries < 7:
+    try:
+        # count attempt as a try
+        tries += 1
+        # ask for input
+        inpt = int(input("Please input a number between 1 and 100: "))
+
+        # check for match
+        if inpt == goal:
+            win = True
+            print("Congrats, you guessed the number!")
+            print("It took you", tries, "tries")
+        # give hints
+        elif inpt < goal:
+            print("The number should be higher")
+        else:
+            print("The number should be lower")
+    except ValueError:
+        print("Please type an integer")
+
+
+if not win:
+    print("Game over! You took more than seven tries")
+
+
+# Tries not getting the string exception even after 7 tries
+# except Value Error - docs of all possible exceptions
+#
