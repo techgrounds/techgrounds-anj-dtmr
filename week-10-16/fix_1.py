6277f4fdd217b9c921ed93bbac52155934e65e61
adtmr <anjdeitmer@gmail.com>
2023-06-02 12:46:48 +0200

    Create fix_1.py

---
+++ week-10-16/fix_1.py
@@ -0,0 +1,16 @@
+# '''
+# The output should be:
+# hello Casper
+# hello Floris
+# hello Esther
+# '''
+# foo = 'hello'
+# ls = ['Casper', 'Floris', 'Esther']
+# for name in ls:
+# 	print(loo,name)
+
+
+loo = 'hello'
+ls = ['Casper', 'Floris', 'Esther']
+for name in ls:
+    print(loo, name)
