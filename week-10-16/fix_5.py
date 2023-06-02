3b6a0dce864d4f50b028439cc6eda09f1da5bcb0
adtmr <anjdeitmer@gmail.com>
2023-06-02 12:30:26 +0200

    Create fix_5.py

---
+++ week-10-16/fix_5.py
@@ -0,0 +1,10 @@
+# '''
+# The output should be:
+# Star Wars
+# '''
+# ls = ['Lord of the rings', 'Star Trek', 'Iron Man', 'Star Wars']
+# print(ls[4])
+
+
+ls = ['Lord of the rings', 'Star Trek', 'Iron Man', 'Star Wars']
+print(ls[3])
