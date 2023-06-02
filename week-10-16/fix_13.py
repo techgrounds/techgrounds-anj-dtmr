d207b11b3943fab19f8316bf5cc44e621d46be2e
adtmr <anjdeitmer@gmail.com>
2023-06-02 11:33:02 +0200

    Create fix_13.py

---
+++ week-10-16/fix_13.py
@@ -0,0 +1,14 @@
+# '''
+# The output should be:
+# Your random number is: <insert random number here>
+# '''
+# import random
+#
+# random.randint(1,100)
+# print("Your random number is:")
+
+import random
+
+random_num = random.randint(1, 100)
+print(f"Your random number is: {random_num}")
+
