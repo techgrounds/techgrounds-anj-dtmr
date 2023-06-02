9fb668b42ebc33720210f6904dccaa8ad0941678
adtmr <anjdeitmer@gmail.com>
2023-06-02 12:31:54 +0200

    Create fix_4.py

---
+++ week-10-16/fix_4.py
@@ -0,0 +1,18 @@
+# '''
+# The output should be:
+# there are 0 kids on the street
+# there are 1 kids on the street
+# there are 2 kids on the street
+# there are 3 kids on the street
+# there are 4 kids on the street
+# '''
+# foo = 0
+# while foo <= 5:
+# 	print('there are', foo, 'kids on the street')
+# 	foo += 1
+
+
+foo = 0
+while foo < 5:
+    print('there are', foo, 'kids on the street')
+    foo += 1
