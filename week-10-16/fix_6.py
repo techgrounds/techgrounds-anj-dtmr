b12fdec0f6e1a55e42ddc443c27e533526f8b028
adtmr <anjdeitmer@gmail.com>
2023-06-02 12:29:38 +0200

    Create fix_6.py

---
+++ week-10-16/fix_6.py
@@ -0,0 +1,25 @@
+# '''
+# The output should be:
+# 80
+# '''
+# foo = 80
+# if foo < 30:
+# 	print(foo)
+# else:
+# 	print('big number wow')
+# elif foo < 100:
+# 	print(foo)
+
+
+'''
+The output should be:
+80
+'''
+foo = 80
+if foo < 30:
+    print(foo)
+elif foo < 100:
+    print(foo)
+else:
+    print('big number wow')
+
