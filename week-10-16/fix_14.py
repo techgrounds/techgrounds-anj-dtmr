fe2084cb47cfeaea8e8b4a54688b99eb13b8e52d
adtmr <anjdeitmer@gmail.com>
2023-06-02 11:33:09 +0200

    Create fix_14.py

---
+++ week-10-16/fix_14.py
@@ -0,0 +1,19 @@
+# '''
+# The output should be:
+# True
+# '''
+# def rtn(x):
+# 	return(x)
+#
+# foo = rtn(3)
+#
+# if foo > rtn(4):
+# 	print(True)
+# else:
+# 	print(False)
+
+
+def rtn(x):
+    foo = rtn(3)
+    if foo < rtn(4):
+        return True
